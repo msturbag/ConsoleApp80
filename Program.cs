16aa448cc214268c00498ea50392970332764666
Vovka0777 <vovabelko07@mail.ru>
2023-12-17 22:55:37 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+
+            //Random a = new Random();
+            //int b = a.Next(-10,10);
+            //Console.WriteLine($"Ваше число - {b}");
+            //switch (b)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Понедельник");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Вторник");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Среда");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Четверг");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Пятница");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Суббота");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Воскресенье");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Ошибка");
+            //        break;
+            //}
+            //Console.Read();
+
+            //Задача 2
+
+            //Random a = new Random();
+            //int b = a.Next(1,100);
+            //int d = a.Next(1,100);
+            //Console.WriteLine($"Первое выпавшее число - {b}");
+            //Console.WriteLine($"Второе выпавшее число - {d}");
+            //int g = 0;
+            //int h = 0;
+            //bool z = b % 2 == 0 & d % 2 == 0;
+            //bool l = b % 2 == 1 | d % 2 == 0;
+            //if (z == true)
+            //{
+            //    g = b / 2;
+            //    h = d / 2;
+            //}
+            //else if (l == false)
+            //{
+            //    g = b * 2;
+            //    h = d * 2;
+            //}
+            //else
+            //{
+            //    g = b + 1;
+            //    h = d + 1;
+            //}
+            //Console.WriteLine($"Получился ответ:\n Первое - {g}\n Второе - {h}");
+            //Console.Read();
+
+            //Задача 3
+
+            //Random c = new Random();
+            //int a = c.Next(0, 100);
+            //Console.WriteLine($"Ученик набрал {a} баллов");
+            //string b = (a >= 90 && a <= 100) ? "отлично" :
+            //               (a >= 70 && a <= 89) ? "хорошо" :
+            //               (a >= 50 && a <= 69) ? "удовлетворительно" :
+            //               "неудовлетворительно";
+            //Console.WriteLine($"Его оценка - {b}");
+            //Console.Read();
+        }
+    }
+}
